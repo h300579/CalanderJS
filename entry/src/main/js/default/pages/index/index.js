b399da098869e6f1da0910d40c8499c674f100d1
h300579 <1193787696@qq.com>
2022-04-11 15:24:22 +0800

    2022年4月11日15:24:07 完成title UI 尝试实现 事件 UI

--- entry/src/main/js/default/pages/index/index.js
+++ entry/src/main/js/default/pages/index/index.js
@@ -1,8 +1,23 @@
 export default {
     data: {
-        title: ""
+        title: "",
+        currentMonth:"",
+        toDoLists:"",
     },
     onInit() {
         this.title = this.$t('strings.world');
+        this.currentMonth = "4月";
+        this.toDoLists = [
+            {
+                title: "todo1",
+                description:"des1,asdfasdfa,afsdfasdf.",
+                data:"2022-4-11"
+            },
+            {
+                title: "todo2",
+                description:"des2,asdfasdfa,afsdfasdf.",
+                data:"2022-4-13"
+            },
+        ]
     }
 }
